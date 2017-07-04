4d8ab3feec807a57c2fbb219582b1f7c2e6179e4
galinovivan <9225913@gmail.com>
2017-07-04 07:23:25 +0300

    port

--- single-portfolio.php
+++ single-portfolio.php
@@ -9,7 +9,36 @@
 			</style>
 			<div class="post-content single-text row">
 				<div class="small-12 columns">
-					<?php the_content(); ?>
+                    <?php $images = get_field('gallery');
+                        if ($images):
+                    ?>
+                            <div id="slider" class="flexslider">
+                                <ul class="slides">
+                                    <?php foreach ($images as $image): ?>
+                                        <li>
+                                            <img src="<?=$image['url'];?>"
+                                        </li>
+                            <?php endforeach; ?>
+                                </ul>
+                            </div>
+                            <div id="carousel" class="flexslider">
+                                <ul class="slides">
+                                    <?php foreach ($images as $image): ?>
+                                        <li>
+                                            <img src="<?=$image['sizes']['portfolio-thumb'];?>"
+                                        </li>
+                                    <?php endforeach; ?>
+                                </ul>
+                            </div>
+                            <?php endif; ?>
+                    <div id="about">
+                        <div class="title">
+                            <?php the_title(); ?>
+                        </div>
+                        <div class="text">
+                            <?php the_field('portfolio_text'); ?>
+                        </div>
+                    </div>
 				</div>
 			</div>
 		</article>
